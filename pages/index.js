b52e1d54f4cdde40697638c7cdaff16b47305d96
RazorGo <85153304+RazorGo@users.noreply.github.com>
2022-03-20 00:40:25 +0530

    fix mobile view bug

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://portfolio-eosin-sigma-51.vercel.app/">Divyanshu.dev</a>
+          Welcome to <a href="https://portfolio-eosin-sigma-51.vercel.app/">Divyanshu</a>
         </h1>
 
         <p className={styles.description}>
